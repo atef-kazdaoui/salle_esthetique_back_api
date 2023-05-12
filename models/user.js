6d25dded6e86cfbe319bd1013f5cb58482fc17e2
atef-kazdaoui <102931021+atef-kazdaoui@users.noreply.github.com>
2023-05-12 14:27:05 +0200

    more security

--- models/user.js
+++ models/user.js
@@ -50,10 +50,14 @@ Clientsmodel.associate=models=>{
     onDelete:"cascade"
     });    
 }
-(async () => {
+/**
+ * (async () => {
   await sequelize.sync({ force: true }); // Synchronisation avec la base de données en créant les tables, cette opération supprime les tables existantes
   console.log('La table User a été synchronisée avec la base de données');
 })();
+
+ * 
+ */
 module.exports = Clientsmodel;
 
 
